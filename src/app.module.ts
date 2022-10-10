d89633fe5a5ddc51b5fb446cabd27ef5edd21dc9
barnaholl <hollbarna@freemail.hu>
2022-10-10 20:49:41 +0200

    add user.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
