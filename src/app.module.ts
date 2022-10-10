cb449a16b9d65422098a7456beffa93d3837fcf1
barnaholl <hollbarna@freemail.hu>
2022-10-10 21:07:10 +0200

    add .env to gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.NODE_ENV === "test" ? process.env.MONGO_DB_TEST_URI : process.env.MONGO_DB_URI
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
