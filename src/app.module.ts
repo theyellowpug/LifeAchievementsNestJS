b0c442bcfca88dd01a9bce128c6b4287afcbe820
barnaholl <hollbarna@freemail.hu>
2022-10-19 21:03:45 +0200

    feat(app.module) connect to mongodb with dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,24 @@ import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
+    UserModule,
     MongooseModule.forRootAsync({
       useFactory: () => ({
-        uri: process.env.NODE_ENV === "test" ? process.env.MONGO_DB_TEST_URI : process.env.MONGO_DB_URI
+        //uri: "mongodb+srv://barnaholl:Linoleum69@lifeachievements.vzcu98l.mongodb.net/life-achivements-db?retryWrites=true&w=majority"
+        uri: process.env.NODE_ENV === "test" ? process.env.MONGO_DB_URI: process.env.MONGO_DB_URI
       }),
     }),
-    UserModule,
+   /* GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),*/
   ],
   controllers: [AppController],
   providers: [AppService, UserService],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UserService, UserResolver],
 })
 export class UserModule {}
